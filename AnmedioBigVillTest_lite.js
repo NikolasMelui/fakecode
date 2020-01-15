4b9b7e74a50a786cf1e7961fdf98ba7464d9b87d
nikolasmelui <slak@samaradom.ru>
2020-01-15 10:08:47 +0400

    Create AnmedioBigVillTest_lite.js

---
+++ AnmedioBigVillTest_lite.js
@@ -0,0 +1,6 @@
+const test = new AnmedioBigVillTest({
+  sticker: true || super(tizer.length),
+  title__prog: test.amazingTest(),
+  tiny: console.info(`Hello, ${user.name}`),
+  size__shadowAllowFakeScreen: 120 / 120,
+});
