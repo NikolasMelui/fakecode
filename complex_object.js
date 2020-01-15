ad5caf48e4df1eff5272e72f2ce1f233a6cdf64f
nikolasmelui <slak@samaradom.ru>
2020-01-15 10:23:24 +0400

    Create complex_object.js

---
+++ complex_object.js
@@ -0,0 +1,18 @@
+const newObj = {
+  first: 1,
+  second: '2',
+  third: [1, 2, 3],
+  4: {
+    5: true,
+    6: [1, 2, 3],
+    7: {
+      8: 8,
+      9: [
+        { firstInner: 1, secondInner: '2', thirdInner: [1, 2, 3] },
+        { firstInner: 1, secondInner: '2' },
+        {},
+      ],
+      10: {},
+    },
+  },
+};
